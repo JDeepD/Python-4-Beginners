804d8f115a76bac40aed64604756e589c47cca01
Saptarshi Adhikari <66560136+Mastermind-sap@users.noreply.github.com>
2020-10-15 22:19:51 +0530

    adding comments
    added some comments to make the algo clear

--- Searching/Binary_search.py
+++ Searching/Binary_search.py
@@ -1,17 +1,19 @@
+# Function to search element x in array1
 def binary_search_program(array1, x): 
-    a = 0
-    b = len(arr) - 1
-    c = 0
+    a = 0 #lower index
+    b = len(arr) - 1 #upper index
+    c = 0 
   
     while a <= b: 
-        c = (b + a) // 2
-        if array1[c] < x: 
-            a = c + 1
+        c = (b + a) // 2 #mid-value index
+        #comparing the mid value to the element that needs to be searched
+        if array1[c] < x:  
+            a = c + 1   
         elif array1[c] > x: 
             b = c - 1       
         else: 
-            return c 
-    return -1
+            return c #returns the index when mid-index value equals the search element
+    return -1 #returns -1 if element not found
   
   
 array1 = [2,3,6,4,45,94,7,9,5,69,89,33,44,67,45] 
