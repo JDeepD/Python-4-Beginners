1a7a9b70ebbab8083f91d26f4cf84a51cd0cd58c
Saptarshi Adhikari <66560136+Mastermind-sap@users.noreply.github.com>
2020-10-16 01:59:06 +0530

    Pascal triangle

---
+++ BasicMathematics/pascal_triangle.py
@@ -0,0 +1,14 @@
+import math
+def c(n, r):
+	return math.factorial(n)/(math.factorial(r)*math.factorial(n-r))
+def col(n):
+	if n==1:
+		return 1
+	else:
+		return n+1	
+row=int(input("rows:  "))
+for i in range(1, row+1):
+	print(end= " "*(row-i))
+	for j in range (col(i)):
+		print(int(c(i, j)), end=" ")
+	print("")
