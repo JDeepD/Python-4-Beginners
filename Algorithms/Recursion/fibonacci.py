4657a96800203b1807741477249321f3f51276b4
adnanhasan251 <68420989+adnanhasan251@users.noreply.github.com>
2020-11-21 01:10:05 +0530

    Update fibonacci.py

--- Algorithms/Recursion/fibonacci.py
+++ Algorithms/Recursion/fibonacci.py
@@ -1,7 +1,7 @@
 #I used swapping technique, so that it does the memory work itself. I am writing a shorter approach to nth term of fibonacci
 
 def fibonacci(n):
-	a,b=0,1 #base is chosem as 0 and 1
+	a,b=0,1 #base is assumed as 0 and 1, so 0,1 and 1 are the 1st, 2nd and 3rd term respectively.
 	for i in range(n-1):
 		a,b=b,a+b
 	return a
